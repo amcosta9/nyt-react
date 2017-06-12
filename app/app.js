c2e25e6bfcc51f0fc2fd71a992a1f4287744eb9d
Ariel <ariel.m.little@gmail.com>
2017-06-11 22:40:27 -0400

    initial site files

---
+++ app/app.js
@@ -0,0 +1,9 @@
+// react dependencies
+let React = require("react");
+let ReactDOM = require("react-dom");
+
+// include Main component
+let Main = require("./components/Main");
+
+// render main component
+ReactDOM.render(<Main />, document.getElementById("app"));
