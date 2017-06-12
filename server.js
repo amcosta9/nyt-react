23795f0e1cd8053fc50f6fca66e569684e62c5e6
Ariel <ariel.m.little@gmail.com>
2017-06-11 22:06:08 -0400

    basic file structure and set up server.js file

---
+++ server.js
@@ -0,0 +1,48 @@
+// load dependencies
+let express = require("express"),
+    bodyParser = require("body-parser"),
+    logger = require("morgan"),
+    mongoose = require("mongoose");
+
+// require Article model
+let Article = require("./models/Article");
+
+// set up express
+let app = express();
+const PORT = process.env.PORT || 3005;
+
+// run morgan for logging
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// serve static content from public dir
+app.use(express.static("./public"));
+
+// mongodb config
+mongoose.connect("mongodb://localhost/nytreact");
+let db = mongoose.connection;
+
+db.on("error", function(err) {
+    console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+    console.log("Mongoose connection successful.");
+});
+
+// landing route
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+// ----------
+// TODO add routes here
+// ----------
+
+// listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
